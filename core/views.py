18357fa1c2c32c49d2231a8781ec839941a8405a
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-10-22 16:07:42 +0530

    acc settings updated

--- core/views.py
+++ core/views.py
@@ -31,11 +31,16 @@ def signup(request):
                 user  = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                #login user and redirecting to settings page
+
+                user_login = auth.authenticate(username=username, password = password)
+                auth.login(request,user_login)
+
                 #creating a profile model for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user = user_model.id)
                 new_profile.save()
-                return redirect('signin') 
+                return redirect('settings') 
         else:
             messages.info(request,'Password not matching')
             return redirect('signup')
@@ -66,4 +71,32 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.profileimg = image
+            user_profile.save()
         
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.profileimg = image
+            user_profile.save()
+            
+        return redirect('settings')
+
+    context = {'user_profile':user_profile}
+    return render(request, 'setting.html', context)
