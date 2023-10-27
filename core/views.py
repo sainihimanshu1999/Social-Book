2be20f4232070c17e8641efa5fa13d6855c44b28
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-10-27 13:58:20 +0530

    project complete

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from . models import Profile, Post, LikePost, FollowerCount
 from itertools import chain
+import random
 
 
 
@@ -14,7 +15,6 @@ from itertools import chain
 def index(request):
     user_object = User.objects.get(username = request.user.username)
     profile_object = Profile.objects.get(user = user_object)
-    posts = Post.objects.all().order_by('-created_at')
 
     user_following_list = []
     feed = []
@@ -29,9 +29,35 @@ def index(request):
         feed.append(feed_lists)
 
     feed_list = list(chain(*feed))
+
+    #user suggetions start
+
+    all_users = User.objects.all()
+    user_following_all = []
+     
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestion_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.get(username=request.user.username)
+    final_suggestions_list =[x for x in list(new_suggestion_list) if (x != current_user)]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_list = Profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_list)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
     
 
-    context = {'profile_object':profile_object,'posts':feed_list}
+    context = {'profile_object':profile_object,'posts':feed_list, 'suggestions_username_profile_list':suggestions_username_profile_list[:4]}
     return render(request,'index.html',context)
 
 def signup(request):
